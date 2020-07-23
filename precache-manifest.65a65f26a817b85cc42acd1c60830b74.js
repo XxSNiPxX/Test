3fba153bc01756a0d1ac352863e8829d65aab273
Rishabh <rishabhhyd@gmail.com>
2020-07-23 18:20:14 +0530

    Updates

---
+++ precache-manifest.65a65f26a817b85cc42acd1c60830b74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cee2a405d1f81b0c3da81195c50b21",
+    "url": "/XxSNiPxX/Test.git/index.html"
+  },
+  {
+    "revision": "e9f15d9aaf72631c4538",
+    "url": "/XxSNiPxX/Test.git/static/css/main.902761ba.chunk.css"
+  },
+  {
+    "revision": "8dbf8aa28ea518879180",
+    "url": "/XxSNiPxX/Test.git/static/js/2.b963579e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/XxSNiPxX/Test.git/static/js/2.b963579e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f15d9aaf72631c4538",
+    "url": "/XxSNiPxX/Test.git/static/js/main.cbd56f58.chunk.js"
+  },
+  {
+    "revision": "38b96c1330ab0b73ffc5",
+    "url": "/XxSNiPxX/Test.git/static/js/runtime-main.061d3efd.js"
+  },
+  {
+    "revision": "2273a3bb123a846e8bc25d3a8e0c4c03",
+    "url": "/XxSNiPxX/Test.git/static/media/SFOuterLimitsDistUpright.2273a3bb.ttf"
+  },
+  {
+    "revision": "0dec5ddb57bac477684c60c026210f1f",
+    "url": "/XxSNiPxX/Test.git/static/media/logo.55795b4f.png"
+  }
+]);
